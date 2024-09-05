65864b1c5a73d262b9877f4afd1d1ea743aff399
FaCami <camilafarroyo16842@hotmail.com>
2024-09-05 00:11:02 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,12 +1,6 @@
 const miDiv = document.getElementById('miDiv');
 const miBoton = document.getElementById('miBoton');
 
-// Evento div
 miDiv.addEventListener('click', () => {
     alert('¡Hola! Soy el div');
-});
-
-// Evento botón
-miBoton.addEventListener('click', () => {
-    alert('¡Hola!');
 });
